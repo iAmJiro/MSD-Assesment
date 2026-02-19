8a4231bd3e630a310ebfcd9dd0e2de459c8026b1
Assessment 🤖 <noreply@assessment.bot>
2026-02-20 01:04:44 +1300

    UnitTest for People

--- csharp/Exercise2.Tests/UnitTest1.cs
+++ csharp/Exercise2.Tests/UnitTest1.cs
@@ -1,10 +1,17 @@
-﻿namespace Exercise2.Tests;
+﻿using Xunit;
+using Review;
+using System;
+using System.Linq;
 
-public class UnitTest1
+namespace Exercise2.Tests
 {
     [Fact]
-    public void Test1()
+    public void Person_Constructor_DefaultsToUtcNow()
     {
-
+        // This test ensures a new person (newborn) is age 0 by default
+        var person = new Person("Newborn");
+    
+        // Check if the DOB is within a few seconds of 'Now'
+        Assert.True(DateTimeOffset.UtcNow.Subtract(person.DOB).TotalSeconds < 5);
     }
 }
