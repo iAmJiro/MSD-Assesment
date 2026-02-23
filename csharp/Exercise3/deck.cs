d0c44b645ea9e6e4686e3e243789b65ccf158aaa
Assessment 🤖 <noreply@assessment.bot>
2026-02-23 19:51:30 +1300

    Shuffle logic added, count for cards added

--- csharp/Exercise3/deck.cs
+++ csharp/Exercise3/deck.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using CardGame.Domain;
 namespace CardGame.Domain
 {
@@ -16,5 +17,20 @@ namespace CardGame.Domain
                 _cards.Add(new Card(s, r));
             }
         }
+    }
+    // Could also be done with a lambda, I just prefer it this way
+    public int RemainingCards 
+    {
+        get 
+        {
+            return _cards.Count;
+        }
+    }
+    public void Shuffle(IShuffler shuffler)
+    {
+        // I'm not too sure if it will ever be null; but I will add a safety check just in case
+        if (shuffler == null) throw new ArgumentNullException(nameof(shuffler));
+        // Shuffles deck using the shuffler from cardGame.cs
+        shuffler.Shuffle(_cards);
     }
 }   
